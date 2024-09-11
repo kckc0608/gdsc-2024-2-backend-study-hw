becbf91ecb46ae455d83ff9428cf0c7e23e19517
에버듀 <everdu@ebeodyuui-MacBookAir.local>
2024-09-11 09:27:24 +0900

    feat: week1 project setting

---
+++ todo-api-hw/src/main/java/com/example/todoapihw/TodoApiHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.todoapihw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApiHwApplication.class, args);
+    }
+
+}
---
+++ todo-api-hw/src/test/java/com/example/todoapihw/TodoApiHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todoapihw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
